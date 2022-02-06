c5530f06e826d3d2bd300f726783fdf1b25060c8
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2022-02-06 11:55:08 +0700

    config: setup redux

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        // root: ['.'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.svg'],
+        alias: {
+          '@assets': './assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@translate': './src/i18n',
+          '@navigators': './src/navigators',
+          '@screens': './src/screens',
+          '@api': './src/services/api',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/redux/store.ts
@@ -0,0 +1,22 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import rootReducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+
+const composeEnhancers = compose;
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let middleware = [thunkMiddleware];
+
+export const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(...middleware)),
+);
+export const persistor = persistStore(store);
